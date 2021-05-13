d97ed43980057730531f18af0290be9561f7cfe4
DakshPatel2007 <79900403+DakshPatel2007@users.noreply.github.com>
2021-05-13 19:15:31 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var database;
+var gameState = 0;
+var playerCount = 0;
+var form,game,player;
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+
+  
+}
+
+function draw(){
+}
